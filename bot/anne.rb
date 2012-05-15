720f1a89e532cd8d2a65bc491997eee6c7aac4b8
spartango@gmail.com <spartango@gmail.com>
2012-05-15 09:56:20 -0700

    added stubs for fuzzy searching and asana calls

--- bot/anne.rb
+++ bot/anne.rb
@@ -11,9 +11,25 @@ module Bot
             @log.level = Logger::DEBUG
         end
 
+        # Asana interactions
+
+        def findWorkspace(workspaceName) 
+            # Fetch workspace listing -> cache
+            # Fuzzy search for workspace
+        end
+
+        def findTask(taskName, workspace)
+            # Fetch tasks from workspace
+
+        end
+
+        # Messaging
+
         def buildMessage(user, body) 
             return Blather::Stanza::Message.new user, body
         end
+
+        # Events
 
         def onStatus(fromNodeName)
             # Dont do anything on status
@@ -32,31 +48,39 @@ module Bot
                 return buildMessage message.from.stripped ("Anne: Hello "+senderName)
 
             # Listing
-
+            # TODO 
             # Creation 
-            
+
             # Tasks must have associated workspace
                 # Single line
                 # "anne, create ... [taskname] ... [workspacename] "
-                    # Fetch workspace listing
-                    # Fuzzy search for workspace
+            elsif condition
+                    workspace = findWorkspace workspaceName
                     # Create task
+                    workspace.create_task(:name => taskName)
+                    return buildMessage message.from.stripped ("Anne: ")
 
+            elsif condition
             # Story
                 # Single line
                 # "anne, ... comment on ... [story] ... [taskname] ... [workspacename]"
-                    # Fetch workspace listing
-                    # Fuzzy search for workspace
+                    workspace = findWorkspace workspaceName
                     # Fuzzy search for task
+                    task = findTask taskName workspace
                     # Create story task
-
+                    task.create_story(:text => commentText)
+                    return buildMessage message.from.stripped ("Anne: ")
+            
+            elsif condition            
             # Completion
                 # Single line
                 # "anne, ... complete ... [taskname] ... [workspacename]"
-                    # Fetch workspace listing
-                    # Fuzzy search for workspace
-                    # Fuzzy search for task
+                    workspace = findWorkspace workspaceName
+                    # Find task
+                    task = findTask taskName workspace
                     # Update task
+                    task.update_attributed(:completed, true)
+                    return buildMessage message.from.stripped ("Anne: ")
 
             else
                 # Default / Give up
