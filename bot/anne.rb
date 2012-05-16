c8a46205be1fdf3da6cf8b674828b0fd396635a9
spartango@gmail.com <spartango@gmail.com>
2012-05-15 23:55:51 -0700

    minor

--- bot/anne.rb
+++ bot/anne.rb
@@ -278,7 +278,7 @@ module Bot
                 return handleCompleteTask message.from.stripped, params[:taskName], params[:workspaceName] if params
 
             elsif queryText.match /help/i
-                [(buildMessage message.from.stripped, "Anne: Hi "+senderName+"! I can help you *list* workspaces, tasks, or projects. "),
+                [(buildMessage message.from.stripped, "Anne: Hi "+senderName+"! I can *list* workspaces, tasks, or projects. "),
                  (buildMessage message.from.stripped, "Anne: I can also help *create tasks* or *complete tasks*, or *post comments*. ")
                  (buildMessage message.from.stripped, "Anne: I'm happy to be of service. ")]
             end
